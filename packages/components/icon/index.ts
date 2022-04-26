7140c228f9c352480b6577c97545fbd6e6517704
liangqifei <1454554357@qq.com>
2022-04-26 15:29:08 +0800

    feat: initinit

--- packages/components/icon/index.ts
+++ packages/components/icon/index.ts
@@ -1,7 +1,8 @@
 import { withInstall } from '@casta-fe-playground/utils/with-install';
 import Icon from './src/icon.vue';
+import {IconProps} from './src/icon';
 
 const TgIcon = withInstall(Icon);
 
-export { TgIcon };
+export { TgIcon,IconProps };
 export default TgIcon;
